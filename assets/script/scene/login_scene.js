6b0960a850538b0e5c1127315bdca64f12abe2e0
zhuangmiaorun <951763028@qq.com>
2019-01-30 18:07:05 +0800

    优化login场景

--- assets/script/scene/login_scene.js
+++ assets/script/scene/login_scene.js
@@ -1,11 +1,16 @@
 cc.Class({
     extends: cc.Component,
 
+    properties: {
+        loading_node: cc.Node,
+        start_game_node: cc.Node
+    },
+
     start() {
         require('Global')();
+        this.start_game_node.active = false;
         tips.show(document.cookie);
         this.scheduleOnce(this.init_user_info, 1);
-        this.node.getChildByName('start_game').active = false;
         cc.director.preloadScene('game');
     },
 
@@ -13,7 +18,8 @@ cc.Class({
         $.post('http://api.cccx.ltd/post', {
             module: 'UserService.userstate'
         }, function (data, status, xhr) {
-            this.node.getChildByName('start_game').active = true;
+            this.start_game_node.active = true;
+            this.loading_node.active = false;
             if (status === 'success') {
                 var ret = data;
                 try {
@@ -29,7 +35,7 @@ cc.Class({
                     Global.UserMgr.login();
                     return;
                 }
-                var tel = String(data.tel)
+                var tel = String(data.tel);
                 cc.js.get(Global.UserMgr, 'id', function () { return tel; });
                 Global.Observer.emit('login', tel);
             } else {
@@ -39,8 +45,8 @@ cc.Class({
     },
 
     start_game() {
-        this.node.getChildByName('start_game').destroy();
-        this.node.getChildByName('loading').active = true;
+        this.start_game_node.destroy();
+        this.loading_node.active = true;
         Global.AudioMgr.play('main_bg', 1, true, 'BGM');                   //背景音乐
         cc.director.loadScene('game');
     }
